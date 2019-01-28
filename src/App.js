7a2ac08c0a5f19a79a65f78df2cd240790801e61
prabhnith <prabhjot.nith@gmail.com>
2019-01-28 17:39:23 +0530

    Groups added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Gallery from "./Components/Gallery/Gallery";
+import Groups from "./Components/Groups/Groups";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Gallery />
+        {/* <Gallery /> */}
+        <Groups />
       </div>
     );
   }
--- src/Components/Groups/Groups.js
+++ src/Components/Groups/Groups.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./Gallery.css";
+import "./Groups.css";
 
 const REACT_APP_API_KEY = "cd7182a2e83351df21c02d6c0e4f2d9e";
 class Gallery extends Component {
@@ -13,6 +13,18 @@ class Gallery extends Component {
   componentDidMount() {
     // alert(REACT_APP_API_KEY);
     //  Each group card will contain its name, avatar and some images of that group
+    fetch(
+      "https://api.flickr.com/services/rest/?method=flickr.groups.search&api_key=" +
+        REACT_APP_API_KEY +
+        "&text=flickrcentral&per_page=10&format=json&nojsoncallback=1"
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(j) {
+        console.log(JSON.stringify(j));
+      });
+
     fetch(
       "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" +
         REACT_APP_API_KEY +
@@ -61,13 +73,24 @@ class Gallery extends Component {
             </div>
           </div>
         </nav>
-        <div className="col-md-12">
+
+        <div className="card">
+          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" />
+          <div className="container">
+            <h4>
+              <b>John Doe</b>
+            </h4>
+            <p>Architect & Engineer</p>
+            <div className="col-md-4">
           <div className="row">
             <hr />
 
             <div className="gal">{this.state.pictures}</div>
           </div>
         </div>
+          </div>
+        </div>
+        
       </div>
     );
   }
