e5a88b5af6214cc3180c6ab64e0510c826a7879b
prabhnith <prabhjot.nith@gmail.com>
2019-01-29 19:47:30 +0530

    fetching the group images

--- src/Components/Groups/Groups.js
+++ src/Components/Groups/Groups.js
@@ -8,17 +8,57 @@ class Groups extends Component {
     this.state = {
       pictures: [],
       searchGroupValue: "",
-      groups: []
+      groups: [],
+      groupID_Images: [],
+      groupIDs:[]
     };
   }
 
-  componentDidMount() {
+  // fetch images from the group
+  async fetchGroupImages(groupID){
+    let groupIDImages={};
+    const response = await fetch("https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=" +
+      REACT_APP_API_KEY + "&group_id=" + groupID + "&per_page=9&format=json&nojsoncallback=1");
+      let j = await response.json();
+      let groupImages = j.photos.photo.map(pic => {
+        var srcPath = "https://farm" + pic.farm + ".staticflickr.com/" + pic.server + "/" + pic.id + "_" + pic.secret + ".jpg";
+        return <img alt="dogs" src={srcPath} />;
+      });
+      // console.log(groupImages);
+      groupIDImages[groupID] = groupImages;
+
+      var newArray = this.state.groupID_Images.slice();    
+      newArray.push(groupIDImages);   
+      this.setState({groupID_Images:newArray});
+      console.log(this.state.groupID_Images);
+      // console.log("FIRST :",this.state.groupID_Images[0][groupID][0].props.src);
+      if(this.state.groupID_Images.length ==9){
+        // this.getImagesfromArray();
+      }
+  }
+
+  getImagesfromArray(){
+    var groupImageArray = this.state.groupID_Images;
+    let groupIDs = this.state.groupIDs;
+    let groupImagesSrc = groupImageArray.map((obj, index) =>{
+      // console.log(index);
+      console.log(obj);
+      console.log(Object.keys(obj)[index]);
+      console.log(obj[Object.keys(obj)[0]][0].props.src);
+      // console.log(obj[index][groupIDs[index]][index].props.src);
+      // return obj[index][groupIDs[index]][index].props.src;  
+      // return <img alt="pic" src={obj[Object.keys(obj)[index]][index].props.src} />  
+    });
+    console.log(groupImagesSrc);
+  }
+  async componentDidMount() {
     // alert(REACT_APP_API_KEY);
     //  Each group card will contain its name, avatar and some images of that group
+    
+    //fetch group name according to the text
     fetch(
-      "https://api.flickr.com/services/rest/?method=flickr.groups.search&api_key=" +
-        REACT_APP_API_KEY +
-        "&text=flickrcentral&per_page=10&format=json&nojsoncallback=1"
+      "https://api.flickr.com/services/rest/?method=flickr.groups.search&api_key=" + REACT_APP_API_KEY +
+        "&text=flickrcentral&per_page=9&format=json&nojsoncallback=1"
     )
       .then(function(response) {
         return response.json();
@@ -26,24 +66,24 @@ class Groups extends Component {
       .then(
         function(j) {
           // console.log(JSON.stringify(j));
+          var groupIDs = [];
+          var groupID_Images ={};
           var groupsArray = j.groups.group.map(group => {
             // group.name group.members
             var groupName = group.name;
             var groupMembers = group.members;
-            console.log(groupName, " ", groupMembers);
+            groupIDs.push(group.nsid);
+            this.fetchGroupImages(group.nsid);
+            // groupID_Images[group.nsid] = groupImages;
+            // console.log(groupName, " ", groupMembers);
             return (
               <div className="col-md-4">
                 <div className="card">
-                  <img
-                    className="avatar"
-                    src="https://www.w3schools.com/howto/img_avatar.png"
-                    alt="Avatar"
-                  />
+                  <img className="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" />
                   <h5 className="groupName">{groupName}</h5>
                   <p className="membersCount">Members : {groupMembers}</p>
                   <div className="container">
-                    <div className="row">
-                      <hr />
+                    <div className="row"> <hr />
                       <div className="gal">{this.state.pictures}</div>
                     </div>
                   </div>
@@ -52,13 +92,17 @@ class Groups extends Component {
             );
           });
 
-          this.setState({ groups: groupsArray });
+          this.setState({ groups: groupsArray, groupIDs });
+          
+          // console.log(groupIDs);
+          // console.log(groupID_Images);
         }.bind(this)
       );
-
+      
+      
+      //fetch random images with tag=face 
     fetch(
-      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" +
-        REACT_APP_API_KEY +
+      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + REACT_APP_API_KEY +
         "&tags=face&per_page=9&page=1&format=json&nojsoncallback=1"
     )
       .then(function(response) {
@@ -68,21 +112,13 @@ class Groups extends Component {
         function(j) {
           //   alert(JSON.stringify(j));
           let picArray = j.photos.photo.map(pic => {
-            var srcPath =
-              "https://farm" +
-              pic.farm +
-              ".staticflickr.com/" +
-              pic.server +
-              "/" +
-              pic.id +
-              "_" +
-              pic.secret +
-              ".jpg";
+            var srcPath = "https://farm" + pic.farm + ".staticflickr.com/" + pic.server + "/" + pic.id + "_" + pic.secret + ".jpg";
             return <img alt="dogs" src={srcPath} />;
           });
           this.setState({ pictures: picArray });
         }.bind(this)
-      );
+      ); 
+
   }
 
   render() {
@@ -106,26 +142,6 @@ class Groups extends Component {
         </nav>
 
         <div className="row groupsContainer">
-          <div className="col-md-4">
-            <div className="card">
-              <img
-                className="avatar"
-                src="https://www.w3schools.com/howto/img_avatar.png"
-                alt="Avatar"
-              />
-              <h4>
-                <b>John Doe</b>
-              </h4>
-              <p>Architect & Engineer</p>
-              <div className="container">
-                <div className="row">
-                  <hr />
-                  <div className="gal">{this.state.pictures}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-
           {this.state.groups}
         </div>
       </div>
