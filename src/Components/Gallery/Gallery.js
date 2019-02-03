d09a28947316e96a703afbb3d216f4eb972179bf
prabhnith <prabhjot.nith@gmail.com>
2019-02-03 12:56:15 +0530

    Search is working on submit.

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,43 +1,39 @@
 import React, { Component } from "react";
 import "./Gallery.css";
-import StackGrid from "react-stack-grid";
+import config from '../../config';
 
-const REACT_APP_API_KEY = "cd7182a2e83351df21c02d6c0e4f2d9e";
 class Gallery extends Component {
   constructor() {
     super();
     this.state = {
-      pictures: []
+      groupGalleryImages: []
     };
   }
 
-  componentDidMount() {
-    // alert(REACT_APP_API_KEY);
-    //  Each group card will contain its name, avatar and some images of that group
-    fetch(
-      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" +
-      REACT_APP_API_KEY +
-      "&tags=nyc&per_page=20&page=1&format=json&nojsoncallback=1")
-      .then(function (response) {
-        return response.json();
-      }).then(
-        function (j) {
-          let picArray = j.photos.photo.map(pic => {
-            var srcPath = "https://farm" + pic.farm + ".staticflickr.com/" + pic.server + "/" + pic.id + "_" + pic.secret +".jpg";
-            return <div key={pic.secret}><img width="100%" alt="dogs" src={srcPath} key={pic.secret} /> </div>;
-          });
-          this.setState({ pictures: picArray });
-        }.bind(this)
-      );
+  componentWillReceiveProps() {
+    let groupIDImages = [];
+    fetch("https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=" +
+      config.REACT_APP_API_KEY + "&group_id=" + this.props.groupID + "&per_page=20&format=json&nojsoncallback=1")
+      .then(response => response.json())
+      .then(j => j.photos.photo.map(pic => {
+        var srcPath = "https://farm" + pic.farm + ".staticflickr.com/" + pic.server + "/" + pic.id + "_" + pic.secret + ".jpg";
+        return <div key={pic.secret}><img alt="pic" width="100%" src={srcPath} key={pic.secret} /> </div>;
+      })).then(grpImages => {
+        groupIDImages.push(grpImages);
+        this.setState({ groupGalleryImages: groupIDImages });
+      }).catch(e => console.error(e));
   }
 
   render() {
     return (
 
-      <StackGrid columnWidth={350} gutterWidth={7}>
-        {this.state.pictures}
-      </StackGrid>
-
+      <div className="gal">
+        {
+          (this.state.groupGalleryImages) ?
+            (this.state.groupGalleryImages) :
+            <p>Loading...</p>
+        }
+      </div>
     );
   }
 }
--- src/Components/Groups/Groups.js
+++ src/Components/Groups/Groups.js
@@ -3,21 +3,13 @@ import React, { Component } from "react";
 import "./Groups.css";
 
 import SearchGroups from "../SearchGroups/SearchGroups";
-import GroupGallery from "../GroupGallery/GroupGallery";
-import Gallery from "../Gallery/Gallery";
 
 class Groups extends Component {
   constructor() {
     super();
     this.state = {
-      inputValue: "",
-      searchWord: ""
     };
   }
-
-  updateInputValue = (inputValue) => { this.setState({ inputValue }); }
-
-  handleSubmitSearch = () => { this.setState({ searchWord: this.state.inputValue }); }
 
   componentDidMount() {
 
@@ -26,8 +18,8 @@ class Groups extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <SearchGroups searchValue={this.setSearchValue} handleSubmit={this.handleSubmitSearch} handleSubmitSearch={this.updateInputValue} />
-        <GroupGallery keyword={this.state.searchWord} />
+        <SearchGroups />
+
       </div>
     );
   }
